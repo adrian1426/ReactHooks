623bc311bf102ca0ed6b0e7b4713914bf0e37add
adrian hernandez <adrianhh1426@gmail.com>
2020-03-29 17:35:40 -0600

    app base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [click,setClick] = useState(0);
+
+  const addClick = () =>{
+    setClick(click + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button 
+        onClick={addClick}
+      >
+        Clicks ({click}) 
+      </button>
     </div>
   );
+
 }
 
 export default App;
