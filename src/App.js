27bf6971a695c06edae41046abda71ded25ffdcf
adrian hernandez <adrianhh1426@gmail.com>
2020-04-19 14:04:11 -0500

    useRef,useState,useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const entradaText = useRef();
+  const [name, setName] = useState('');
+  const [productos, setProductos] = useState([]);
+  const refInput = useRef();
+
+  const handleInput = e => {
+    setName(e.target.value)
+  };
+
+  useEffect(() => {
+    //debounce
+    setTimeout(() => {
+      if (name === refInput.current.value) {
+        fetch(`https://universidad-react-api-test.luxfenix.now.sh/products?name=${name}`)
+          .then(res => res.json())
+          .then(data => setProductos(data.products));
+      }
+    }, 600);
+  }, [name]);
 
   return (
     <div className="App">
       <input
         type='text'
-        placeholder='ingresa tu texto'
-        ref={entradaText}
+        placeholder='ingresa tu búsqueda'
+        ref={refInput}
+        onChange={handleInput}
       />
-      <button onClick={() => entradaText.current.focus()}>Focus</button>
-      <button onClick={() => entradaText.current.blur()}>Blur</button>
+      <ul>
+        {
+          productos.map((product, i) => (
+            <li key={i}>{product.name}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
