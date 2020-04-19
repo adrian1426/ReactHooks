79daed9148a781de457d8932d6fb5e6b09a0eed9
adrian hernandez <adrianhh1426@gmail.com>
2020-04-18 23:29:07 -0500

    useRef

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
-import React, {useState, useContext} from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 
-//{Provider y Consumer}
-const MyContext = React.createContext();
-
-//Context de forma tradicional
-const Nieto = () => (
-  <MyContext.Consumer>
-    {(context) => (
-      <div>
-        <h1>Nieto {context.add}</h1>
-        <button onClick={context.onClickAdd}>click Nieto</button>
-      </div>
-    )}
-  </MyContext.Consumer>
-);
-
-//context con hook useContext
-const Nieto2 = () => {
-  const {add, onClickAdd} = useContext(MyContext);
-  return (
-    <div>
-      <h1>Nieto2 {add}</h1>
-      <button onClick={onClickAdd}>click Nieto</button>
-    </div>
-  );
-};
-
-const Hijo = () => (
-  <div>
-    <h1>Hijo</h1>
-    <Nieto />
-    <Nieto2 />
-  </div>
-);
-
 function App() {
-  const [add, setAdd] = useState(0);
-
-  const onClickAdd = () => setAdd(add + 1);
+  const entradaText = useRef();
 
   return (
-    <MyContext.Provider
-      value={{
-        add,
-        onClickAdd,
-      }}
-    >
-      <div className="App">
-        <button onClick={onClickAdd}>Agregar {add} </button>
-        <Hijo />
-      </div>
-    </MyContext.Provider>
+    <div className="App">
+      <input
+        type='text'
+        placeholder='ingresa tu texto'
+        ref={entradaText}
+      />
+      <button onClick={() => entradaText.current.focus()}>Focus</button>
+      <button onClick={() => entradaText.current.blur()}>Blur</button>
+    </div>
   );
 }
 
