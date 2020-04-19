f1a59f3d7491f2a952bcfdd11c4a946c7111d773
adrian hernandez <adrianhh1426@gmail.com>
2020-04-18 23:11:46 -0500

    useContext

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React, {useState, useEffect, useLayoutEffect} from 'react';
+import React, {useState, useContext} from 'react';
 import './App.css';
+
+//{Provider y Consumer}
+const MyContext = React.createContext();
+
+//Context de forma tradicional
+const Nieto = () => (
+  <MyContext.Consumer>
+    {(context) => (
+      <div>
+        <h1>Nieto {context.add}</h1>
+        <button onClick={context.onClickAdd}>click Nieto</button>
+      </div>
+    )}
+  </MyContext.Consumer>
+);
+
+//context con hook useContext
+const Nieto2 = () => {
+  const {add, onClickAdd} = useContext(MyContext);
+  return (
+    <div>
+      <h1>Nieto2 {add}</h1>
+      <button onClick={onClickAdd}>click Nieto</button>
+    </div>
+  );
+};
+
+const Hijo = () => (
+  <div>
+    <h1>Hijo</h1>
+    <Nieto />
+    <Nieto2 />
+  </div>
+);
 
 function App() {
   const [add, setAdd] = useState(0);
 
-  useEffect(() => {
-    console.log('useEffect 1');
-  }, [add]);
-
-  useEffect(() => {
-    console.log('useEffect 2');
-  }, [add]);
-
-  useLayoutEffect(() => {
-    console.log('useLayoutEffect 1');
-  }, [add]);
-
-  useLayoutEffect(() => {
-    console.log('useLayoutEffect 2');
-  }, [add]);
-
   const onClickAdd = () => setAdd(add + 1);
 
   return (
-    <div className="App">
-      <button onClick={onClickAdd}>Agregar {add} </button>
-    </div>
+    <MyContext.Provider
+      value={{
+        add,
+        onClickAdd,
+      }}
+    >
+      <div className="App">
+        <button onClick={onClickAdd}>Agregar {add} </button>
+        <Hijo />
+      </div>
+    </MyContext.Provider>
   );
 }
 
