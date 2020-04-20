bd244e79416f5fe010971476d1d37880930b683d
adrian hernandez <adrianhh1426@gmail.com>
2020-04-19 22:26:23 -0500

    useCallback con dependencias

--- src/App.js
+++ src/App.js
@@ -20,15 +20,21 @@ const Button = React.memo(({ callback, children }) => {
 
 function App() {
   const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
 
   const incrementA = useCallback(() => {
     setA(a => a + 1);
   }, []);
 
+  const incrementB = useCallback(() => {
+    setB(b => b + a);
+  }, [a]);
+
   return (
     <div className="App">
-      <Button callback={incrementA}>Inrementio</Button>
-      <h1>{a}</h1>
+      <Button callback={incrementA}>InrementioA</Button>
+      <Button callback={incrementB}>InrementioB</Button>
+      <h1>a: {a} -- b: {b}</h1>
     </div>
   );
 }
